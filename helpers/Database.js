a3ed7d01fc9735090398c904cef3cf19b1c5c882
Ayron Little <ayron.little@shopify.com>
2020-04-11 19:42:41 -0400

    query water entries

--- helpers/Database.js
+++ helpers/Database.js
@@ -6,28 +6,44 @@ export const init = () => {
     db.transaction(txn => {
         txn.executeSql(
             `create table if not exists water_entries (
-            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            drinkable VARCHAR(30),
-            amount INTEGER,
-            created_at DATETIME default current_timestamp)`,
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                drinkable VARCHAR(30),
+                amount INTEGER,
+                created_at DATETIME default current_timestamp
+            );`,
             [],
             (txn, res) => { },
             (_, error) => console.log("Error creating water_entries table", error)
         );
         txn.executeSql(
             `create table if not exists water_settings (
-            id INTEGER PRIMARY KEY NOT NULL,
-            name VARCHAR(30),
-            value VARCHAR(30));`,
+                id INTEGER PRIMARY KEY NOT NULL,
+                name VARCHAR(30),
+                value VARCHAR(30)
+            );`,
             [],
             (txn, res) => { },
             (_, error) => console.log("Error creating water_settings table", error)
         );
+
+        // add fake data
         txn.executeSql(
             "insert into water_entries (drinkable, amount) values ('water', ?)",
-            [Math.floor(Math.random() * 1000)],
+            [Math.floor(Math.random() * 100)],
             (txn, res) => { },
             (_, error) => console.log("Error inserting water_entries table", error)
+        );
+        txn.executeSql(
+            "insert into water_entries (drinkable, amount, created_at) values ('week old water', ?, date('now','weekday 0'))",
+            [Math.floor(Math.random() * 100)],
+            (txn, res) => { },
+            (_, error) => console.log("Error inserting week water_entries table", error)
+        );
+        txn.executeSql(
+            "insert into water_entries (drinkable, amount, created_at) values ('month old water', ?, date('now','start of month'))",
+            [Math.floor(Math.random() * 100)],
+            (txn, res) => { },
+            (_, error) => console.log("Error inserting month water_entries table", error)
         );
     },
         error => console.log("transaction error")
@@ -46,15 +62,34 @@ export const querySettings = () => {
     });
 }
 
-export const queryEntries = entry_date_range => {
-    db.transaction(txn => {
-        txn.executeSql(
-            "SELECT * FROM water_entries",
-            [],
-            (txn, res) => { },
-            (_, error) => console.log("Error: ", error)
-        );
-    });
+export const queryEntries = startDate => {
+    let startDateSql = "'start of day'"
+    switch (startDate) {
+        case "week":
+            // 0 = Sunday, 1 = Monday
+            startDateSql = "'start of day','weekday 0'"
+            break; 
+        case "month":
+            startDateSql = "'start of month'"
+            break;
+        default:
+            startDateSql = "'start of day'"
+    }
+
+    const promise = new Promise((resolve, reject) => {
+        db.transaction(txn => {
+            txn.executeSql(
+                "SELECT * FROM water_entries where created_at > date('now',?)",
+                [startDateSql],
+                (txn, res) => {
+                    resolve(res.rows._array)
+                },
+                (_, error) => console.log("Entry query error: ", error)
+            );
+        });
+    })
+
+    return promise
 }
 
 export const addToDailyDrinkTotal = num => {
